57db3ee8323e2c9baf366ce83d2fa4a76024128b
grafikomsen <grafikomsen@gmail.com>
2026-03-06 11:51:29 +0000

    LoginPage Insertion du logo Gmail commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,18 +8,18 @@
         <link rel="stylesheet" href="{{ asset('frontend-assets/fontawesome/css/all.min.css') }}" />
     </head>
     <body class="bg-gray-300">
-        <div  class="mx-auto my-14 max-w-sm sm:my-20 border-2 p-8 rounded-md shadow-md bg-[#222F3D]">
-            <a href="{{ route('home') }}" class=" flex justify-center">
+        <div  class="mx-auto my-12 max-w-sm border-2 p-8 rounded-md shadow-md bg-[#222F3D]">
+            <a href="{{ route('home') }}" class="flex justify-center ">
                 <img class="w-[150px]" src="{{ asset('frontend-assets/images/amazon_logo.png') }}" alt="Logo">
             </a>
-            <h3 class="text-white text-lg py-2">S'identifier</h3>
+            <h3 class="py-2 text-lg text-white">Se connecter</h3>
             <form action="{{ route('login') }}" method="POST">
                 @csrf
                 <div class="grid grid-cols-1 gap-x-8 gap-y-4">
 
                     <!-- Email -->
                     <div>
-                        <label for="email" class="block text-sm/6 font-semibold text-white">Email</label>
+                        <label for="email" class="block font-semibold text-white text-sm/6">Email</label>
                         <div class="mt-1">
                             <input type="email" name="email" :value="old('email')" class="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" />
                         </div>
@@ -30,7 +30,7 @@
 
                     <!-- Password -->
                     <div>
-                        <label for="password" class="block text-sm/6 font-semibold text-white">Password</label>
+                        <label for="password" class="block font-semibold text-white text-sm/6">Password</label>
                         <div class="mt-1">
                             <input type="password" name="password" class="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" />
                         </div>
@@ -43,22 +43,25 @@
                         <!-- Remember Me -->
                         <div class="block mt-2">
                             <label for="remember_me" class="inline-flex items-center">
-                                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                                <span class="ms-2 text-sm text-gray-200">Remember me</span>
+                                <input id="remember_me" type="checkbox" class="text-indigo-600 border-gray-300 rounded shadow-sm focus:ring-indigo-500" name="remember">
+                                <span class="text-xs text-gray-200 ms-2">Remember me</span>
                             </label>
                         </div>
 
                         <div>
                             @if (Route::has('password.request'))
-                                <a class="underline text-sm text-gray-200 hover:text-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                                <a class="text-xs text-gray-200 underline rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                                     Forgot your password?
                                 </a>
                             @endif
                         </div>
                     </div>
-                    <button class="bg-orange-500 text-white p-2 rounded-md" type="submit">Se connecter</button>
-                    <a class="bg-orange-500 text-white p-2 rounded-md text-center" href="" >Se connecter avec google</a>
-                    <a href="{{ route('register') }}" class="text-end text-xs text-white">Nouveau chez Amazon ? créer votre compte Amazon</a>
+                    <button class="p-2 text-white bg-orange-500 rounded-md" type="submit">Se connecter</button>
+                    <div class="flex items-center justify-center gap-2 p-2 border border-orange-500 rounded-md">
+                        <img class="w-6" src="{{ asset('frontend-assets/images/google.svg') }}" alt="">
+                        <a class="p-2 text-xs text-center text-orange-500 rounded-md" href="" >Se connecter avec google</a>
+                    </div>
+                    <a href="{{ route('register') }}" class="text-xs text-white text-end">Nouveau chez Amazon ? créer votre compte Amazon</a>
                 </div>
             </form>
         </div>
