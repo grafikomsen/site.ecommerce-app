e0d8b8a3fec23544da766b0ea7844a6ef6ca1a04
grafikomsen <grafikomsen@gmail.com>
2026-04-03 08:41:35 +0000

    Autoload: Configuration de la page App/Helpers/helper.php commit

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -3,15 +3,15 @@
 use App\Models\Category;
 use App\Models\ProductImage;
 
-    function getCategories(){
+function getCategories(){
 
-        return Category::orderBy('name_fr','ASC')
-                ->with('sub_categories')
-                ->where('showHome','Oui')
-                ->where('status','1')
-                ->get();
-    }
+    return Category::orderBy('name_fr','ASC')
+            ->with('sub_categories')
+            ->where('showHome','Oui')
+            ->where('status','1')
+            ->get();
+}
 
-    function getProductImage($productId){
-        return ProductImage::where('product_id',$productId)->first();
-    }
+function getProductImage($productId){
+    return ProductImage::where('product_id',$productId)->first();
+}
